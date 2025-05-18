6d68f1084278f849522a3a39a6ee345eb859e261
Moiz Moiz <admin@moizmoiz.com>
2025-05-18 19:08:28 +0500

    minor improvements

--- server.js
+++ server.js
@@ -4,21 +4,17 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 
 dotenv.config();
-
 connectDB();
 
-// Initialize Express
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(cors({
-  origin: '*', // Frontend URL
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true // Enable credentials (cookies, authorization headers)
+  credentials: true
 }));
 
-// Routes
 app.use('/api/education', require('./routes/educationRoutes'));
 app.use('/api/skills', require('./routes/skillRoutes'));
 app.use('/api/projects', require('./routes/projectRoutes'));
@@ -27,7 +23,6 @@ app.use('/api/experience', require('./routes/experienceRoutes'));
 app.get('/', (req, res) => {
   res.send('Portfolio API is running...');
 });
-
 
 const PORT = process.env.PORT || 5000;
 
